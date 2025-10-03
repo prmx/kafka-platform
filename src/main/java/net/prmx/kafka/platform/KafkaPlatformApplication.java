6d0eca261c83a74dbea97d18e176abced8e43e0c
Denys Karabetskyi <denis@prmx.net>
2025-10-03 17:17:34 +0200

    Initial commit from Specify template

---
+++ src/main/java/net/prmx/kafka/platform/KafkaPlatformApplication.java
@@ -0,0 +1,13 @@
+package net.prmx.kafka.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPlatformApplication.class, args);
+    }
+
+}
